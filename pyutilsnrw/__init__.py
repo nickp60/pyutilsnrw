73693634f4a7ba658a34a07c580e7778b1c3433e
nickp60 <nickp60@gmail.com>
2016-11-04 13:34:52 +0000

    changed init

--- pyutilsnrw/__init__.py
+++ pyutilsnrw/__init__.py
@@ -1,5 +1,5 @@
 ##  Package version
 # should match r"^__version__ = '(?P<version>[^']+)'$" for setup.py
 # use single quotes to surround version
-__version__ = '0.0.761'
+__version__ = '0.0.762'
 #import utils3_5
